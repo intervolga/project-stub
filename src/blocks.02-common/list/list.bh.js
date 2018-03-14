ed8e4bbb648d522fdb1200ad26127c2d3e1e7420
zvezdilin <artem@intervolga.ru>
2018-03-14 15:39:05 +0300

    Блок "list" исправлена ошибка примера, модифицирован bh

--- src/blocks.02-common/list/list.bh.js
+++ src/blocks.02-common/list/list.bh.js
@@ -2,7 +2,7 @@ module.exports = function (bh) {
     bh.match('list', function (ctx, json) {
         ctx.tag('ul').content(
             ctx.content().map((item)=>{
-                if(typeof item !== 'string') return item;
+                if(typeof item === 'object') return item;
                 return {elem: 'li', content: item}
             }),
             true
